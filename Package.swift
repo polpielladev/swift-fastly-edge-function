46812f5977f2cfea1ce4bab932f64a6365b65450
Pol Piella Abadia <hi@polpiella.dev>
2023-01-14 11:10:08 +0000

    Adds `Compute@Edge` package as a dependency

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 let package = Package(
     name: "swift-fastly-edge-function",
     dependencies: [
+        .package(url: "https://github.com/swift-cloud/Compute", from: "2.8.0")
     ],
     targets: [
         .executableTarget(
             name: "SwiftFastlyEdgeFunction",
-            dependencies: []
+            dependencies: ["Compute"]
         )
     ]
 )
