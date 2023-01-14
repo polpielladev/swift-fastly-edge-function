6c6e445bf5d643976edca5a10ddadaa9e6a4169a
Pol Piella Abadia <hi@polpiella.dev>
2023-01-14 11:05:42 +0000

    Creates empty executable Swift Package

---
+++ Tests/swift-fastly-edge-functionTests/swift_fastly_edge_functionTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_fastly_edge_function
+
+final class swift_fastly_edge_functionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_fastly_edge_function().text, "Hello, World!")
+    }
+}
