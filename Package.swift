4c4875a0d18007b98d9d28c3ad187152892ec999
Pol Piella Abadia <hi@polpiella.dev>
2023-01-14 11:11:56 +0000

    Creates a hello world compute runtime

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-fastly-edge-function",
+    platforms: [.macOS(.v11)],
     dependencies: [
         .package(url: "https://github.com/swift-cloud/Compute", from: "2.8.0")
     ],
--- Sources/SwiftFastlyEdgeFunction/main.swift
+++ Sources/SwiftFastlyEdgeFunction/main.swift
@@ -1,1 +1,6 @@
+import Compute
 
+try await onIncomingRequest { req, res in
+    let text = "Hello World!"
+    try await res.status(200).send(text)
+}
